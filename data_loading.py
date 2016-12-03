dbe611e8708466880a454bf9bfbae2b3b283d0b6
David Modjeska <modjeska@acm.org>
2016-12-03 14:47:36 -0500

    Removed extra profit stuff.

--- data_loading.py
+++ data_loading.py
@@ -3,7 +3,7 @@
 
 # # Data Loading
 
-# In[105]:
+# In[25]:
 
 import itertools as it
 import numpy as np
@@ -22,7 +22,7 @@ from scipy.io import mmread
 from IPython.display import display, HTML
 
 
-# In[106]:
+# In[26]:
 
 ### specify processed data files to generate - full/partial, partial %, and train/test
 ### Note: this cell is the same in both notebooks
@@ -58,12 +58,12 @@ term_freqs_file = dir_str + "term_freqs_" + mode_str + pct_str + ".mtx"
 diff_terms_file = dir_str + "diff_terms_" + mode_str + pct_str + ".json"
 
 
-# In[107]:
+# In[27]:
 
 processed_data_train_file
 
 
-# In[108]:
+# In[28]:
 
 ### load processed data
 data = pd.read_json(processed_data_train_file)
@@ -75,12 +75,12 @@ count_cols_df = pd.read_json(diff_terms_file)
 count_cols_bool = count_cols_df.values > 0.0
 
 
-# In[109]:
+# In[29]:
 
 print len(data)
 
 
-# In[110]:
+# In[30]:
 
 model_loan_term = 36
 data_filtered = data[data.loan_term == model_loan_term]
@@ -88,18 +88,18 @@ data_filtered = data_filtered[pd.to_datetime(data_filtered.issue_date).dt.year.i
 print len(data_filtered)
 
 
-# In[111]:
+# In[31]:
 
 pd.to_datetime(data_filtered.issue_date).dt.year.value_counts()
 
 
-# In[112]:
+# In[32]:
 
 # del x['verif_status']
 data_filtered.describe().T
 
 
-# In[113]:
+# In[33]:
 
 # earliest_credit is not really a good indicator -- we want to know how long has elapsed since then
 # See http://stackoverflow.com/questions/17414130/pandas-datetime-calculate-number-of-weeks-between-dates-in-two-columns
@@ -108,31 +108,32 @@ data_filtered['months_since_earliest_credit'] = (
 ).round()
 
 
-# In[114]:
+# In[34]:
 
 data_filtered.columns
 
 
-# In[115]:
+# In[35]:
 
-data_filtered_x = data_filtered.drop('loan_status', 1)
+data_filtered_x = data_filtered.drop('loan_status', axis = 1)
 data_filtered_y = data_filtered['loan_status']
 
 
-# In[116]:
+# In[36]:
 
 # copy unstandardized columns for later profit calculation
 profit_data = data_filtered_x[['installment', 'loan_amount', 'recoveries']]
 recoveries_avg = profit_data.recoveries.sum() / float(np.count_nonzero(profit_data.recoveries))
+data_filtered_x = data_filtered_x.drop('recoveries', axis = 1)
 
 
-# In[117]:
+# In[38]:
 
 def expand_x(x, x_orig):
     x_expanded = pd.DataFrame()
     for colname in x_orig.columns:
         if colname in ('description', 'verif_status', 'loan_subgrade', 'id', 'interest_rate'
-                       'index', 'recoveries', 'issue_date', 'earliest_credit'):
+                       'index', 'issue_date', 'earliest_credit'):
             continue
         print colname, x_orig[colname].dtype
         if x_orig[colname].dtype == 'object':
@@ -151,24 +152,24 @@ def expand_x(x, x_orig):
     return x_expanded
 
 
-# In[118]:
+# In[39]:
 
 data_filtered_expanded_x = expand_x(data_filtered_x, data_filtered_x)
 
 
-# In[119]:
+# In[40]:
 
 data_filtered_expanded_x.describe().T
 
 
-# In[120]:
+# In[41]:
 
 data_filtered_expanded_x.columns
 
 
 # ### Split Data
 
-# In[121]:
+# In[42]:
 
 # Get a more manageable sample
 np.random.seed(1729)
@@ -184,21 +185,21 @@ x_test_expanded = data_filtered_expanded_x.iloc[~sample_flags, :]
 print len(x_test_expanded)
 
 
-# In[122]:
+# In[43]:
 
 # split response column
 y = data_filtered_y.iloc[sample_flags]
 y_test = data_filtered_y.iloc[~sample_flags]
 
 
-# In[123]:
+# In[44]:
 
 # split profit data
 profit_data_train = profit_data.iloc[sample_flags, :]
 profit_data_test = profit_data.iloc[~sample_flags, :]
 
 
-# In[124]:
+# In[45]:
 
 ### filter NLP data
 filter_flags = data_nlp.loan_term.values == model_loan_term
@@ -211,7 +212,7 @@ desc_matrix_filtered = desc_matrix[filter_flags]
 count_cols_bool_filtered = count_cols_bool[filter_flags]
 
 
-# In[125]:
+# In[46]:
 
 ### split NLP data into training and testing sets
 np.random.seed(1729)
@@ -233,7 +234,7 @@ years_nlp = pd.to_datetime(x_nlp_train.issue_date).dt.year
 years_nlp_test = pd.to_datetime(x_nlp_test.issue_date).dt.year
 
 
-# In[126]:
+# In[47]:
 
 ### match indexes
 
@@ -244,19 +245,19 @@ count_cols_bool_train.index = x_nlp_train.index
 count_cols_bool_test.index = x_nlp_test.index
 
 
-# In[127]:
+# In[48]:
 
 # inspect test proportion of good/bad loans
 y_test.value_counts()
 
 
-# In[128]:
+# In[49]:
 
 # verify size of train set
 np.count_nonzero(x_expanded.loan_amount)
 
 
-# In[129]:
+# In[50]:
 
 # be prepared to split stuff up by year of issue
 years = pd.to_datetime(data_filtered_x.issue_date.iloc[sample_flags]).dt.year
@@ -265,7 +266,7 @@ years_test = pd.to_datetime(data_filtered_x.issue_date.iloc[~sample_flags]).dt.y
 
 # ### Apply PCA to predictors
 
-# In[130]:
+# In[51]:
 
 tsvd = tSVD(n_components = 100)
 data_filtered_expanded_x_pca = pd.DataFrame(tsvd.fit_transform(data_filtered_expanded_x))
@@ -273,7 +274,7 @@ data_filtered_expanded_x_pca.index = data_filtered_expanded_x.index
 pca_cum_var_expl = np.cumsum(np.round(tsvd.explained_variance_ratio_, 4) * 100)
 
 
-# In[131]:
+# In[52]:
 
 print "PCA: first and last columns where % variance explained >= 99:",             np.where(pca_cum_var_expl >= 99)[0][[0, -1]]
 
@@ -281,7 +282,7 @@ x_expanded_pca = data_filtered_expanded_x_pca.iloc[sample_flags, :73]
 x_test_expanded_pca = data_filtered_expanded_x_pca.iloc[~sample_flags, :73]
 
 
-# In[132]:
+# In[53]:
 
 tsvd = tSVD(n_components = 500)
 desc_matrix_filtered_pca = pd.DataFrame(tsvd.fit_transform(desc_matrix_filtered))
@@ -289,49 +290,13 @@ desc_matrix_filtered_pca.index = x_nlp_filtered.index
 pca_cum_var_expl = np.cumsum(np.round(tsvd.explained_variance_ratio_, 4) * 100)
 
 
-# In[133]:
+# In[54]:
 
 print "PCA: first and last columns where % variance explained >= 85:",             np.where(pca_cum_var_expl >= 85)[0][[0, -1]]
 
 
-# In[134]:
+# In[55]:
 
 desc_matrix_pca = desc_matrix_filtered_pca.iloc[train_flags, :]
 desc_matrix_test_pca = desc_matrix_filtered_pca.iloc[~train_flags, :]
-
-
-# #### Predict recoveries
-
-# In[135]:
-
-# predictors = pd.concat((profit_data.loan_amount, profit_data.installment), 
-#                        axis = 1)
-# predictors_bad = predictors[data_filtered_y == True]
-# response_bad = profit_data.recoveries[data_filtered_y == True]
-
-# train_flags = np.random.random(predictors_bad.shape[0]) < 0.7
-# local_x_train = predictors_bad.iloc[train_flags, :]
-# local_y_train = response_bad[train_flags]
-# local_x_test = predictors_bad.iloc[~train_flags, :]
-# local_y_test = response_bad[~train_flags]
-
-# model = Lin_Reg().fit(local_x_train, local_y_train)
-# print "R-squared on test set =", model.score(local_x_test, local_y_test)
-
-# # predict on bad loans
-# pred_recover = model.predict(local_x_test)
-# pred_recover_sum = pred_recover.sum()
-# actual_sum = local_y_test.sum()
-
-# print "Difference between predicted and actual recoveries:"
-# diff = np.abs(pred_recover_sum - actual_sum)
-# print "\tAbsolute =", round(diff)
-# print "\tRelative =", round(diff / float(actual_sum), 4)
-
-# print "Average predicted vs. actual recoveries:"
-# print "\tPredicted =", round(pred_recover_sum / float(response_bad.shape[0]))
-# print "\tActual =", round(actual_sum / float(response_bad.shape[0]))
-
-# # predict on all loans
-# predicted_recoveries = model.predict(predictors)
 

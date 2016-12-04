a8e4810dd3e08b8809769dc6a47babb96d95cdbf
Andrew Greene <agreene@adobe.com>
2016-12-04 07:02:24 -0500

    Added ROC curves

--- modelling_framework.py
+++ modelling_framework.py
@@ -50,6 +50,27 @@ def calc_expected_profit(profit_data_test, test_y_hat):
     profit_mm = round(interest_revenue + recoveries - principal_losses) / float(10 ** 6)
     return profit_mm
 
+def ROC_plot(model, X, Y, model_name):
+    # Plot the ROC curve for the given model
+    roc_data = []
+    for p in np.arange(0, 1, 0.01):
+        yhat = model.predict_proba(X)[:,0] <= p
+        false_positive_rate = ((yhat == 1) & (Y == 0)).sum() * 1.0 / ((Y == 0).sum())
+        true_positive_rate = ((yhat == 1) & (Y == 1)).sum() * 1.0 / ((Y == 1).sum())
+        roc_data.append((false_positive_rate, true_positive_rate))
+        # mark the key thresholds that we might use
+        if p in (0.5, 0.6, 0.85):
+            plt.scatter(false_positive_rate, true_positive_rate)  
+
+    plt.plot([roc[0] for roc in roc_data],
+             [roc[1] for roc in roc_data],
+            )
+    plt.xlim(-.01, 1.0)
+    plt.ylim(0, 1.01)
+    plt.title("ROC Curve for " + model_name)
+    plt.xlabel("False Positive Rate")
+    plt.ylabel("True Positive Rate")
+    plt.show()
 
 # In[2]:
 
@@ -82,61 +103,55 @@ def eval_model_all_years(model_factory,
         x_local = pd.DataFrame(poly_xform.fit_transform(x_local))
         x_local_test = pd.DataFrame(poly_xform.fit_transform(x_local_test))
         
-    if True: # for yr in [2011, 2012, 2013]: # set(years.values):
-        indexes = range(len(years))
-        np.random.shuffle(indexes)
+    indexes = range(len(years))
+    np.random.shuffle(indexes)
 
-        cm_accum = np.zeros((2, 2))
-        f1_accum = 0
-        score = 0
-        weighted_score = 0
+    cm_accum = np.zeros((2, 2))
+    f1_accum = 0
+    score = 0
+    weighted_score = 0
 
-        # k-fold cross-validation
-        for i in range(k):
-            train_indexes = list(indexes[0:len(indexes)*i/k]) + list(indexes[len(indexes)*(i+1)/k:])
-            test_indexes = indexes[len(indexes)*i/k:len(indexes)*(i+1)/k]
-        
-            #print "TRAIN ", train_indexes
-            #print 'TEST', test_indexes
-            #print "Y", y.iloc[test_indexes]
-            
-            # model = model_factory().fit(x_expanded[years==yr], y[years==yr])
-            # score = model.score(x_expanded[years==yr], y[years==yr]) / k
-            model = model_factory().fit(x_local.iloc[train_indexes,:], y.iloc[train_indexes])
-            y_hat = model.predict(x_local)
-            score += model.score(x_local.iloc[test_indexes], y.iloc[test_indexes]) / k
-            y_hat_weighted = (model.predict_proba(x_local)[:,0] > prob_threshold)[test_indexes]
-            weighted_score += (y.iloc[test_indexes][y_hat_weighted]).mean() / k
-            cm_accum += confusion_matrix(y.iloc[test_indexes], y_hat[test_indexes])
-            f1_accum += f1_score(y.iloc[test_indexes], y_hat[test_indexes], pos_label = 1) / k
+    # k-fold cross-validation
+    for i in range(k):
+        train_indexes = list(indexes[0:len(indexes)*i/k]) + list(indexes[len(indexes)*(i+1)/k:])
+        test_indexes = indexes[len(indexes)*i/k:len(indexes)*(i+1)/k]
 
-            if model_name is None:
-                model_name = type(model).__name__
-        
-        # but also test against the x_test
-        test_y_hat = (model.predict_proba(x_local_test)[:,0] > prob_threshold)
-        test_score = (y_test == test_y_hat).mean()
-        test_precision = 1- y_test[test_y_hat].mean()
-        test_f1 = f1_score(y_test, test_y_hat, pos_label = 1)
+        model = model_factory().fit(x_local.iloc[train_indexes,:], y.iloc[train_indexes])
+        y_hat = model.predict(x_local)
+        score += model.score(x_local.iloc[test_indexes], y.iloc[test_indexes]) / k
+        y_hat_weighted = (model.predict_proba(x_local)[:,0] > prob_threshold)[test_indexes]
+        weighted_score += (y.iloc[test_indexes][y_hat_weighted]).mean() / k
+        cm_accum += confusion_matrix(y.iloc[test_indexes], y_hat[test_indexes])
+        f1_accum += f1_score(y.iloc[test_indexes], y_hat[test_indexes], pos_label = 1) / k
 
-        # expected profit
-        profit_mm = calc_expected_profit(profit_data_test, test_y_hat)
-        
-        print "all   score: %.3f  baseline: %.3f   1-prec: %.3f   f1: %.3f  | test score %.3f  1-prec %.3f f1 %.3f  GP %dMM" % (
-            score, 1-y.mean(), 1-weighted_score, f1_accum, test_score, test_precision, test_f1, profit_mm)
+        if model_name is None:
+            model_name = type(model).__name__
 
-        model_performance[model_name] = {
-            'score': score,
-            'baseline' : 1-y.mean(),
-            'prec' : 1-weighted_score,
-            'f1': f1_accum,
-            'test_score': test_score,
-            'test_prec': test_precision,
-            'test_f1': test_f1,
-            'test_profit': profit_mm,
-        }
+    # but also test against the x_test
+    model = model_factory().fit(x_local, y)
+    test_y_hat = (model.predict_proba(x_local_test)[:,0] > prob_threshold)
+    test_score = (y_test == test_y_hat).mean()
+    test_precision = 1- y_test[test_y_hat].mean()
+    test_f1 = f1_score(y_test, test_y_hat, pos_label = 1)
 
-# TODO: Confusion matrix (right now, we're not doing well enough to worry about that)
+    # expected profit
+    profit_mm = calc_expected_profit(profit_data_test, test_y_hat)
+
+    ROC_plot(model, x_local_test, y_test, model_name)
+
+    print "all   score: %.3f  baseline: %.3f   1-prec: %.3f   f1: %.3f  | test score %.3f  1-prec %.3f f1 %.3f  GP %dMM" % (
+        score, 1-y.mean(), 1-weighted_score, f1_accum, test_score, test_precision, test_f1, profit_mm)
+
+    model_performance[model_name] = {
+        'score': score,
+        'baseline' : 1-y.mean(),
+        'prec' : 1-weighted_score,
+        'f1': f1_accum,
+        'test_score': test_score,
+        'test_prec': test_precision,
+        'test_f1': test_f1,
+        'test_profit': profit_mm,
+    }
 
 
 # In[194]:

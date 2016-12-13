e2a149e412b53fcc2349a1871ae38cfb7a83a11d
Andrew Greene <agreene@adobe.com>
2016-12-13 12:01:50 -0500

    Better prep of model properties, more GBC data

--- model_performance.py
+++ model_performance.py
@@ -12,11 +12,22 @@ for line in open('model_performance.txt', 'r'):
     d = json.loads(line)
     model_performance[d['name']] = d['perf']
 
+baseline_values = model_performance['LogReg_CV balanced']
+
 h = open('docs/model_performance.html', 'w')
 for k, d in sorted(model_performance.items()):
     s = "<tr><th>%s</th>" % (k, )
     for k2, v in sorted(d.items()):
-        s += "<td>%.3f</td>" % (v,)
+        if k2 in ('baseline', 'test_profit'):
+            continue
+        base_value = round(baseline_values[k2], 3)
+        if k2 == 'test_score':
+            base_value = 0.848
+            
+        if k2 in ('auc', 'test_f1', 'test_prec', 'test_score') and v != 'nan' and round(v, 3) > base_value:
+            s += "<td class='better'>%.3f</td>" % (v, )
+        else:
+            s += "<td>%.3f</td>" % (v, )
     s += "</tr>\n"
     h.write(s)
 h.close()

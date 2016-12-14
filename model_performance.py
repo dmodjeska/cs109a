38f4cce251357b99cfe8e37e1613d342e81e171f
Andrew Greene <agreene@adobe.com>
2016-12-14 10:36:00 -0500

    Started from scratch to recompute precision

--- model_performance.py
+++ model_performance.py
@@ -7,46 +7,58 @@ import pandas as pd
 
 plt.style.use('ggplot')
 
-print "loading"
 model_performance = {}
-for line in open('model_performance.txt', 'r'):
-    d = json.loads(line)
-    model_performance[d['name']] = d['perf']
+for filename in ('model_performance_2.txt',
+                 'model_performance_2a.txt',
+                 'model_performance_2b.txt',
+                 'model_performance_2c.txt',
+#                 'model_performance_2d.txt',
+             ):
+    print "loading", filename
+    for line in open(filename, 'r'):
+        d = json.loads(line)
+        model_performance[d['name']] = d['perf']
 
 baseline_values = model_performance['LogReg_CV balanced']
 
 print "html"
-h = open('docs/model_performance.html', 'w')
+h = open('docs/model_performance_table.js', 'w')
 for k, d in sorted(model_performance.items()):
     s = "<tr><th>%s</th>" % (k, )
-    for k2, v in sorted(d.items()):
-        if k2 in ('baseline', 'test_profit'):
+    for k2 in ('auc', 'f1', 'prec', 'score', 'test_f1_inv', 'test_prec_inv', 'test_score',) :
+        v = d[k2]
+    
+        if k2 in ('baseline', 'test_profit', 'elapsed', 'timestamp', 'model_group'):
             continue
-        base_value = round(baseline_values[k2], 3)
-        if k2 == 'test_score':
-            base_value = 0.848
+        try:
+            base_value = baseline_values[k2]
+        except:
+            continue
+        if k2 != 'auc':
+            base_value = round(model_performance['Always 1'][k2], 3)
             
-        if k2 in ('auc', 'test_f1', 'test_prec', 'test_score') and v != 'nan' and round(v, 3) > base_value:
+        if k2 in ('auc', 'test_f1_inv', 'test_prec_inv', 'test_score') and round(v, 3) > base_value:
             s += "<td class='better'>%.3f</td>" % (v, )
         else:
             s += "<td>%.3f</td>" % (v, )
-    s += "</tr>\n"
-    h.write(s)
+    s += "</tr>"
+    h.write('document.write("' + s + '")\n')
 h.close()
 
 model_performance_df = pd.DataFrame(model_performance).T
-for col in model_performance_df.columns:
+for col in ('auc', 'test_score', 'test_f1_inv', 'test_prec_inv'):
     print col
     base_value = baseline_values[col]
-    if col == 'test_score':
-        base_value = 0.848
+    if col != 'auc':
+        base_value = model_performance['Always 1'][col]
     plt.figure(figsize=(15, .25*len(model_performance_df)))
+    plt.axvline(x=base_value, color='darkgrey')
     sorted_values = model_performance_df[col].fillna(0).sort_values()
     sorted_values.plot(kind='barh', 
-                       color=['kr'[i] for i in list(sorted_values > base_value)],
+                       color=[['darkblue', 'orange'][i] for i in list(sorted_values.apply(lambda x: round(x, 3)) > round(base_value, 3))],
                        width=0.85)
-    plt.title(col)
-    plt.savefig('docs/images/score_' + col + '.png',
+    plt.title(col.replace('_inv', ''))
+    plt.savefig('docs/images/score_' + col.replace('_inv', '') + '.png',
                 bbox_inches='tight')
 
     
